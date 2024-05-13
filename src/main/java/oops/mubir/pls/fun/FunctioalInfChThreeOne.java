d2596aa8b063fae2d81332bfb7e82795e4e0d541
mubir <info.mubir@gmail.com>
2024-05-14 00:45:01 +0900

    may 14

---
+++ src/main/java/oops/mubir/pls/fun/FunctioalInfChThreeOne.java
@@ -0,0 +1,41 @@
+package oops.mubir.pls.fun;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+public class FunctioalInfChThreeOne {
+    static List<Product> getData() {
+        return new ArrayList<Product>() {{
+            add(new Product(1, "one", 10.11));
+            add(new Product(2, "two", 20.22));
+            add(new Product(3, "three", 30.33));
+        }};
+    }
+
+    static Optional<Product> filterProduct(List<Product> products, Predicate<Product> predicate) {
+        for (Product x : products) {
+            if (predicate.test(x))
+                return Optional.of(x);
+        }
+        return Optional.empty();
+    }
+
+    public static void main(String[] args) {
+        List<Product> items = getData();
+
+        // Optional<Product> result = filterProduct(items, item -> item.getPrice() > 25);
+        //if (result.isPresent())
+        //    result.get();
+
+        filterProduct(items, item -> item.getPrice() > 25)
+                .map(product -> product.getName().toUpperCase())//map from optional
+                .ifPresentOrElse(//consumer , runnable
+                        name -> System.out.println(name),
+                        () -> System.out.println("bad luck no match")
+                );
+
+    }
+
+}
---
+++ src/main/java/oops/mubir/pls/fun/Product.java
@@ -0,0 +1,24 @@
+package oops.mubir.pls.fun;
+
+public class Product {
+    int id;
+    String name;
+    double price;
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
