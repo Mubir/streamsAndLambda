bd91ab4670020c8117f7cb1b3d84f55b11316513
mubir <info.mubir@gmail.com>
2024-05-25 12:55:34 +0900

    f

---
+++ src/main/java/oops/mubir/pls/fun/FunctioalInfCh4_7.java
@@ -0,0 +1,50 @@
+package oops.mubir.pls.fun;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FunctioalInfCh4_7 {
+    public static void main(String[] args) {
+        List<FProduct> data = ExampleData.getFProducts();
+
+        List<String> pName = new ArrayList<>();
+
+        /** this is violation of FP as manipulating outside var
+         will create side effect and won't work for parallel stream.
+         */
+        data.stream()
+                .filter(it -> it.getCategory() == Category.FOOD)
+                .map(it -> it.getName())
+                .forEach(it -> pName.add(it));
+
+        System.out.println(" side effect ::  \n" + pName);
+
+        List<String> pNameWithoutSideEffect = data.stream()
+                .filter(it -> it.getCategory() == Category.FOOD)
+                .map(it -> it.getName())
+                .collect(Collectors.toList());
+        System.out.println("no side effect ::  \n" + pNameWithoutSideEffect);
+
+        String joinedName = data.stream()
+                .filter(it -> it.getCategory() == Category.FOOD)
+                .map(it -> it.getName())
+                .collect(Collectors.joining(","));
+
+        System.out.println("All name joined ::  \n" + joinedName);
+
+        /**
+         *  .map(FI:function)
+         *  useage: extract (getXXX())
+         *          manipulate ("\""+myString+"\"")
+         * */
+        String joinedCategory = data.stream()
+                .map(it -> it.getCategory())
+                .distinct()
+                .map(it -> it.toString())
+                .map(it -> "\"" + it + "\"")
+                .collect(Collectors.joining(","));
+
+        System.out.println("All category joined ::  \n" + joinedCategory);
+    }
+}
